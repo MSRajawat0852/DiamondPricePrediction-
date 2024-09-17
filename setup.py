49af62053e3f555ecf3f670f70680043ce8b088b
MSRajawat0852 <141935321+MSRajawat0852@users.noreply.github.com>
2024-09-18 01:24:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Krish',
-    author_email='krish.naik@pw.live',
+    author='manjeet',
+    author_email='manjeet.singh.min21@itbhu.ac.in',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
